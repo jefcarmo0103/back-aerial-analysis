630b9e7f64f8d4b785eaa0a55282c5d6e0e2bf8f
Jefferson Vieira <082160019@ftt.cefsa.edu.br>
2020-06-09 14:37:03 -0300

    Ajustes do login

--- controllers/api/login.controller.js
+++ controllers/api/login.controller.js
@@ -1,7 +1,7 @@
 var config = require('config.json');
 var express = require('express');
 var router = express.Router();
-var userService = require('services/cliente.service');
+var userService = require('services/login.service');
 
 // routes
 router.post('/authenticate', authenticateUser);
